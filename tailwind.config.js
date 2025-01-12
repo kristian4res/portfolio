e13230cf3d73a12ee459e4a2e932eaaf3f48ba16
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-01-12 22:18:53 +0000

    configs

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './src/**/*.vue',
+    './src/**/*.svelte',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
