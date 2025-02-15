4263e5e3b183221cf657a3bd26f96d9dd5504686
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-02-15 18:43:39 +0000

    fix: remove obsolete files; refactor profile subtext

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -9,7 +9,7 @@ defineProps<{
     <div class="flex flex-col justify-center items-center gap-4">
       <h1 class="w-full font-bold text-4xl text-center lg:text-left">{{ title }}</h1>
       <h3 class="w-full text-lg break-words text-center lg:text-left">
-        A software engineer, who's curious about all things related to tech 👨‍💻.
+        <slot></slot>
       </h3>
     </div>
   </div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProjectsView from '../views/ProjectsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,17 +11,9 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/articles',
-      name: 'articles',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ArticlesView.vue'),
-    },
-    {
       path: '/projects',
       name: 'projects',
-      component: () => import('../views/ProjectsView.vue'),
+      component: ProjectsView,
     },
   ],
 })
