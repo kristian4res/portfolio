8f2871f84fe40ee7f796671e73d4940e3bb44fa7
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-02-16 21:42:12 +0000

    test: add view page snapshots

---
+++ src/views/__tests__/HomeView.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import HomeView from '@/views/HomeView.vue'
+
+describe('HomeView.vue', () => {
+  it('renders correctly', () => {
+    const wrapper = mount(HomeView)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
