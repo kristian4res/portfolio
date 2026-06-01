84aec9b9321eee92f6f9df8d5a60044963516fd0
kristian4res <torreskristian77@gmail.com>
2026-06-01 22:33:28 +0100

    todo: fix pfp

--- src/components/ProfileNavHeader.vue
+++ src/components/ProfileNavHeader.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router'
 import ProfileCard from './ProfileCard.vue'
 </script>
 
 <template>
   <header class="flex flex-col justify-center items-center max-w-screen-sm mt-12 mb-2 md:flex-row">
-    <img
+    <!-- <img
       alt="Profile picture of Kristian Torres"
       class="block rounded-full object-cover mb-8 lg:mb-0 lg:mr-4"
-      src="@/assets/profile-pic.jpg"
+      src="@/assets/profile-pic.png"
       width="200"
       height="200"
-    />
+    /> -->
     <div class="flex flex-col items-center lg:flex-row lg:items-start lg:flex-wrap">
       <ProfileCard title="Kristian Torres">
         <p>
-          <b>Software engineer. Problem solver. Always learning.</b><br />
-          I build full-stack applications, work with cloud infrastructure, apply DevSecOps
-          principles, and genuinely enjoy the process.
+          A self-motivated software engineer 👨‍💻 with experience in developing full-stack
+          applications, maintaining multicloud distributed systems and applying DevSecOps
+          principles, within agile multidisciplinary teams.
         </p>
       </ProfileCard>
+      <nav class="w-full text-center mt-8 lg:text-left lg:ml-[-1rem] lg:mt-4">
+        <RouterLink to="/" class="inline-block px-4 py-2 border-l border-gray-300 first:border-0"
+          >Home</RouterLink
+        >
+        <RouterLink to="/projects" class="inline-block px-4 py-2 border-l border-gray-300"
+          >Projects</RouterLink
+        >
+      </nav>
     </div>
   </header>
 </template>
