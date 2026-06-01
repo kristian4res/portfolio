56b58e071fdd1413b73edee75330a2ed9ffe1837
Kristian T. <57638182+kristian4res@users.noreply.github.com>
2026-06-01 22:40:28 +0100

    Update ProfileNavHeader.vue

--- src/components/ProfileNavHeader.vue
+++ src/components/ProfileNavHeader.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
 import ProfileCard from './ProfileCard.vue'
 </script>
 
@@ -8,26 +7,18 @@ import ProfileCard from './ProfileCard.vue'
     <!-- <img
       alt="Profile picture of Kristian Torres"
       class="block rounded-full object-cover mb-8 lg:mb-0 lg:mr-4"
-      src="@/assets/profile-pic.png"
+      src="@/assets/profile-pic.jpg"
       width="200"
       height="200"
     /> -->
     <div class="flex flex-col items-center lg:flex-row lg:items-start lg:flex-wrap">
       <ProfileCard title="Kristian Torres">
         <p>
-          A self-motivated software engineer 👨‍💻 with experience in developing full-stack
-          applications, maintaining multicloud distributed systems and applying DevSecOps
-          principles, within agile multidisciplinary teams.
+          <b>Software engineer. Problem solver. Always learning.</b><br />
+          I build full-stack applications, work with cloud infrastructure, apply DevSecOps
+          principles, and genuinely enjoy the process.
         </p>
       </ProfileCard>
-      <nav class="w-full text-center mt-8 lg:text-left lg:ml-[-1rem] lg:mt-4">
-        <RouterLink to="/" class="inline-block px-4 py-2 border-l border-gray-300 first:border-0"
-          >Home</RouterLink
-        >
-        <RouterLink to="/projects" class="inline-block px-4 py-2 border-l border-gray-300"
-          >Projects</RouterLink
-        >
-      </nav>
     </div>
   </header>
 </template>
